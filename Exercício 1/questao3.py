fcbab751c59284e188369ed388f2f6498ada6c49
Pedro Ian <pedroianmoraes@gmail.com>
2020-03-17 14:17:03 -0300

    atualização da atividade

--- Exercício 1/questao3.py
+++ Exercício 1/questao3.py
@@ -1,3 +1,23 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # definir uma função geradora de grau 2, assim como passado no pdf
+
+def funcaoG(x):
+    return (0.5 * np.square(x) + 3 * x + 10)
+
+def ruidoGaussiano(x):
+    return (x + np.random.normal(0, 4))
+
+
+xAxesBase = np.linspace(-15, 10, 20)
+
+yReal = []
+yResult = []
+for x in xAxesBase:
+    yReal.append(funcaoG(x))
+    yResult.append(ruidoGaussiano(yReal[-1]))
+
+# plt.plot(xAxesBase, yReal)
+# plt.plot(xAxesBase, yResult)
+# plt.show()
