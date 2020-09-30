cb8aa410fae5690755ccb2d5cd78c61e6ee1a977
Pedro Ian <pedroianmoraes@gmail.com>
2020-09-30 10:56:15 -0300

    update

--- Exercicio7/treinaRBF.R
+++ Exercicio7/treinaRBF.R
@@ -1,16 +1,16 @@
 
 rm(list=ls())
 library('corpcor')
+library(mlbench)
 
 fnormal1var <- function(x,m,r) ((1/(sqrt(2*pi*r*r)))*exp(-0.5*((x-m)/(2*r))^2))
 
 fnormal2var <- function(x, m, r) ((1/(sqrt((2 * pi)^length(x) * (det(r))))) * exp(-0.5 *(t(x - m) %*% (solve(r)) %*% (x - m))))
 
-rbfSinc <- function() {
+rbfSinc <- function(centros) {
   xl <- -10
   xr <- 10
-  st <- 1
-  p <- 10
+  p <- centros
   
   faixa <- (xr - xl)/p
   
@@ -43,9 +43,16 @@ rbfSinc <- function() {
   yhat <- H %*% w
   yhatr <- Hrange %*% w
   
-  plot(xrange, yhatr)
+  erro <- 0
+  for (i in 1:length(yhat)) {
+    erro <- erro+(y[i] - yhat[i])^2
+  }
+  erro <- erro/length(yhat)
+  print(erro)
+  
+  plot(xrange, yhatr,xlim = c(-10, 10), ylim = c(-0.5, 1.5))
   par(new=T)
-  plot(x, y)
+  plot(x, y, xlim = c(-10, 10), ylim = c(-0.5, 1.5))
 }
 
 trainaRBF <- function(xIn, yIn, centros, kMediasObj) {
@@ -103,4 +110,59 @@ getResult <- function(xIn, modeloRBF){
   Yhat <- Haug %*% W
   
   return(Yhat)
+}
+
+treinarTodasAsClasses <- function(kCentros) {
+  cores <- rainbow(6)
+  
+  dnormals <- mlbench.2dnormals(200)
+  xor <- mlbench.xor(100)
+  circle <- mlbench.circle(100)
+  spirals <- mlbench.spirals(100,sd = 0.05)
+  
+  allEntrada <- list(dnormals, xor, circle, spirals)
+  
+  for(class in allEntrada) {
+    xIn <- class$x
+    yIn <- as.numeric(class$classes)
+    
+    for (i in 1:length(yIn)) {
+      if (yIn[i] == yIn[1]) {
+        yIn[i] <- 1
+      }
+      else {
+        yIn[i] <- -1
+      }
+    }
+    
+    kMediasReturn <- kmeans(xIn, kCentros, 100)
+    centros <- kMediasReturn$centers
+    
+    objetoTreinamentoRBF <- trainaRBF(xIn, yIn, centros, kMediasReturn)
+    
+    yHatTreinamento <- getResult(xIn, objetoTreinamentoRBF)
+    
+    
+    seqx1x2 <- seq(-4, 4, 0.1)
+    lseq <- length(seqx1x2)
+    MZ <- matrix(0, nrow = lseq, ncol = lseq)
+    
+    for (i in 1:lseq) {
+      for(j in 1:lseq) {
+        x1 <- seqx1x2[i]
+        x2 <- seqx1x2[j]
+        x1x2 <- matrix((cbind(x1, x2)), nrow = 1)
+        MZ[i, j] <- getResult(x1x2, objetoTreinamentoRBF)
+      }
+    }
+    
+    plot(xIn[,1],xIn[,2], col = cores[yIn+2], xlim = c(-4,4), ylim = c(-4,4), xlab='x', ylab='y')
+    par(new = TRUE)
+    contour(seqx1x2, seqx1x2, MZ, nlevels = 1, xlim = c(-4, 4), ylim = c(-4, 4))
+    
+    Sys.sleep(3)
+    persp3D(seqx1x2,seqx1x2,MZ,counter=T,theta = 55, phi = 30, r = 40,d = 0.1, expand = 0.5, ltheta = 90, lphi = 180, shade = 0.4,ticktype = 'detailed', nticks=5)
+    
+  }
+  
 }
